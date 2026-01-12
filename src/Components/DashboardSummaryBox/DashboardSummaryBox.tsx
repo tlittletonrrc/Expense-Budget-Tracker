3e2cec1fa7f555fd00624a97f019eb9e604a1ee1
TarrynBerez <tbereznycky@rrc.ca>
2026-01-12 12:42:47 -0600

    Created dashboard summary box component

--- src/Components/DashboardSummaryBox/DashboardSummaryBox.tsx
+++ src/Components/DashboardSummaryBox/DashboardSummaryBox.tsx
@@ -1,11 +1,29 @@
-import "./Footer.css";
+import "./DashboardSummaryBox.css";
 
-function Footer() {
+function DashboardSummaryBox() {
     return(
     <div> 
-        
+        <table>
+            <tr>
+                <th>Account Balance</th>
+                <th>Left To Spend</th>
+                <th>Payments Due</th>
+            </tr>
+
+            <tr>
+                <td>
+                    $2000
+                </td>
+                <td>
+                    $1000
+                </td>
+                <td>
+                    January 1st, 2026
+                </td>
+            </tr>
+        </table>
     </div>
 );
 }
 
-export default Footer;
+export default DashboardSummaryBox;
