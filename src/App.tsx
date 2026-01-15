0d36060fd4f71a73bc259c49ce6f54eb996b0762
tlittleton-rrc <tlittleton@academic.rrc.ca>
2026-01-15 20:36:09 +0000

    fix deployment issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
