53af1df3d55c6e31be7945763e2f4edac16a35c4
Thomas Littleton <tlittleton@academic.rrc.ca>
2026-01-18 14:02:37 -0600

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
+import Navbar from './Components/Navbar/navbar';
 import AccountsOverview from "./Components/AccountsOverview/accountsOverview"
 import DashboardSummaryBox from './Components/DashboardSummaryBox/DashboardSummaryBox';
 
@@ -8,6 +9,8 @@ function App() {
   return (
     <>
       <Header/>
+
+      <Navbar/>
 
       <DashboardSummaryBox/>
 
---
+++ src/Components/Navbar/navbar.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import './Navbar.css';
+
+interface NavItem {
+  id: number;
+  label: string;
+  path: string;
+}
+
+const Navbar: React.FC = () => {
+  const navItems: NavItem[] = [
+    {
+      id: 1,
+      label: 'Dashboard',
+      path: '/dashboard'
+    },
+    {
+      id: 2,
+      label: 'Accounts',
+      path: '/accounts'
+    },
+    {
+      id: 3,
+      label: 'Categories',
+      path: '/categories'
+    },
+    {
+      id: 4,
+      label: 'Expenses',
+      path: '/expenses'
+    },
+    {
+      id: 5,
+      label: 'Savings',
+      path: '/savings'
+    },
+    {
+      id: 6,
+      label: 'Reports',
+      path: '/reports'
+    }
+  ];
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-header">
+        <h2>expense budget tracker</h2>
+      </div>
+
+      <ul className="nav-list">
+        {navItems.map((item) => (
+          <li key={item.id} className="nav-item">
+            <a href={item.path} className="nav-link">
+              {item.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="navbar-footer">
+        <p>v1.0</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
