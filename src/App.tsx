c9024bbe41cab0c040c9ca6973d60341e385b6b5
TarrynBerez <tbereznycky@rrc.ca>
2026-01-26 16:48:48 -0600

    Added use state for routes and user info

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css'
+import { useState } from 'react';
+import userdata from "./Data/user.json"
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Navbar from './Components/Navbar/navbar';
 import AccountsOverview from "./Components/AccountsOverview/accountsOverview"
 import DashboardSummaryBox from './Components/DashboardSummaryBox/DashboardSummaryBox';
 
+export type UserType = {
+    id: string,
+    name: string,
+    email: string,
+    balance: number,
+    goal: number,
+    due: string
+}
+
 function App() {
+  const [user, setUser] = useState<UserType>(userdata)
+  const [route, setRoute] = useState("/")
+
   return (
     <>
       <Header/>
