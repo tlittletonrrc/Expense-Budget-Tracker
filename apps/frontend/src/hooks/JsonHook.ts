56b608e1e2415df02240b6ba29526a15a2f659ee
TarrynBerez <tbereznycky@rrc.ca>
2026-03-12 22:12:59 -0500

    Removed allocation imports from jsonhooks

--- apps/frontend/src/hooks/JsonHook.ts
+++ apps/frontend/src/hooks/JsonHook.ts
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import * as allocationService from "../Services/AllocationService"
 import * as accountService from "../Services/AccountsService";
 
 /**
@@ -44,16 +43,12 @@ export function useUserProfileDisplay(user: {
     const [balance, setBalance] = useState(user.balance);
     const [savingsGoal, setSavingsGoal] = useState(user.savingsGoal);
     //const [paymentsDue, setPaymentsDue] = useState(user.paymentsDue);
-    /*const [allocations, setAllocations] = useState(user.allocations);*/
     const [accounts, setAccounts] = useState(user.accounts);
 
     const updateSavingsGoal = (newGoal: number) => {
         setSavingsGoal(newGoal);
     };
 
-    // const addPaymentDue = (paymentDate: string) => {
-    //     setPaymentsDue(paymentDate); 
-    // };
 
     const updateAccountBalance = (accountNumber: string, newBalance: number) => {
         setAccounts((prev) =>
@@ -88,54 +83,17 @@ export function useUserProfileDisplay(user: {
         accountService.deleteAccountService(index);
     };
 
-
-    // const addAllocation = (UserID: string, newAllocation: {
-    //     userID: string,
-    //     allocation_id: string
-    //     category: string;
-    //     amount: number;
-    //     date: string;
-    // }) => {
-    //     setAllocations(prev => {
-    //         const exists = prev.some(a => a.category === newAllocation.category);
-
-    //         if (exists) {
-    //             return prev.map(a =>
-    //                 a.category === newAllocation.category
-    //                     ? { ...a, amount: newAllocation.amount, date: newAllocation.date }
-    //                     : a
-    //             );
-    //         }
-
-    //         return [...prev, newAllocation];
-    //     });
-    //     allocationService.createAllocationService(UserID, newAllocation)
-    // };
-
-    // const deleteAllocation = (UserID: string, index: number) => {
-    //     setAllocations(prev => prev.filter((_, i) => i !== index));
-
-    //     allocationService.deleteAllocationService(UserID, index)
-    // };
-
     return {
         userID: user.userID,
         name: user.name,
         email: user.email,
         balance,
         savingsGoal,
-        /*paymentsDue,
-        allocations,*/
         accounts,
-
         setBalance,
         updateSavingsGoal,
-        /*addAllocation,
-        deleteAllocation,
-        addPaymentDue,*/
         updateAccountBalance,
         addAccount,
         deleteAccount,
-        
     };
 }
