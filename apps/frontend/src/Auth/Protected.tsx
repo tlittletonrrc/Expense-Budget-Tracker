0481bd388e16aa3dbebb5ea157ea3504f176bea6
TarrynBerez <tbereznycky@rrc.ca>
2026-04-17 21:13:30 -0500

    Added documentation to protected route component

--- apps/frontend/src/Auth/Protected.tsx
+++ apps/frontend/src/Auth/Protected.tsx
@@ -2,7 +2,13 @@ import { ReactNode } from "react";
 import { useAuth } from "@clerk/clerk-react";
 import { Navigate } from 'react-router-dom';
 
-
+/**
+ * A component that restricts access to signed out users
+ * Redirects user to the login url if not signed in
+ * 
+ * @param children - The react component to show if the user is signed in
+ * @returns - The protected component 
+ */
 export function ProtectedRoute({ children  }: { children : ReactNode }) {
     const { isLoaded, userId } = useAuth();
 
